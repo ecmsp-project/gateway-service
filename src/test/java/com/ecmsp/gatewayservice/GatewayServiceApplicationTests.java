800098a5184f498b75980d01837c0dcaad62bcdd
pawlowiczf <pawlowicz.filip137@gmail.com>
2025-09-24 19:37:31 +0200

    create folder structure, add docker support

---
+++ src/test/java/com/ecmsp/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecmsp.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
